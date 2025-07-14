454e91d562f8d3c80a312a382b70e9daf7ff7485
Dhruv <dhruv261dhiman@gmail.com>
2025-07-14 18:27:00 +0530

    Improvements and edge case scenarios

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,6 @@
-export { MagicAssert } from "./lib/magic-assert.js";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MagicAssert = void 0;
+var magic_assert_js_1 = require("./lib/magic-assert.js");
+Object.defineProperty(exports, "MagicAssert", { enumerable: true, get: function () { return magic_assert_js_1.MagicAssert; } });
 //# sourceMappingURL=index.js.map
